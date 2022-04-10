355a7f1a25109cbd3111712997a48be1b942d868
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-09 20:27:47 -0400

    First, override the onSaveInstanceState method for the main activity to save the user's information when the application is not in the foreground or if the state of the physical device is changed. Secondly, created constant variables for the Keys that are used to save the revenue and dessertsSold information. Finally in the onCreate function, added the check of whether or not the activity has been destroyed or just stopped. If the activity was not destroyed, rather sent to the background, when the onCreate is called, it will retrieve the information passed on by the onSaveInstanceState function. If the activity was destroyed it well fail the check.

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -29,6 +30,9 @@ import com.example.android.dessertclicker.databinding.ActivityMainBinding
 
 //    Constant tag for Log
 const val TAG = "MainActivity"
+//Keys for saving and retrieving data from the instance state bundle
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
 
 class MainActivity : AppCompatActivity() {
 
@@ -73,6 +77,12 @@ class MainActivity : AppCompatActivity() {
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        if(savedInstanceState != null){
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            showCurrentDessert()
+        }
+
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
         }
@@ -113,6 +123,13 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState Called")
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
     }
 
     /**
